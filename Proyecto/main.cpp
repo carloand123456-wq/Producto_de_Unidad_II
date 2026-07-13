0b3ba0ec3af503f72b1a254cd8ccb76b2bdc3c4a
carloand123456-wq <carloand123456@gmail.com>
2026-07-13 10:19:02 -0500

    Arreglando el menu principal.

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 int main(){
 	int op;
-	do{	
-		cout<<"============= MENU PRINCIPAL ============="<<endl;	
-		cout<<"1.- Registro de ventas."<<endl;
-		cout<<"2.- Solicitar un restock."<<endl;
-		cout<<"3.- Salir del programa."<<endl;
-		cout<<"=========================================="<<endl;
+	SetConsoleOutputCP(CP_UTF8);
+	do{
+		cout<<"╔═════════════════════════════════════════╗"<<endl;
+		cout<<"║              MENU PRINCIPAL             ║"<<endl;
+		cout<<"╠═════════════════════════════════════════╣"<<endl;
+		cout<<"║ 1.- Registro de ventas.                 ║"<<endl;
+		cout<<"║ 2.- Solicitar un restock.               ║"<<endl;
+		cout<<"║ 3.- Salir del programa.                 ║"<<endl;
+		cout<<"╚═════════════════════════════════════════╝"<<endl;
 		cout<<"\nEliga una opcion: ";
 		cin>>op;
-		cout<<""<<endl;
-		system("pause");
 		system("cls");
 		
 		switch(op){
 			case 1:
-				cout<<"Ejecutando caso 1..."<<endl;
+				cout<<"╔═════════════════════════════════════════╗"<<endl;
+				cout<<"║          Ejecutando caso 1...           ║"<<endl;
+				cout<<"╚═════════════════════════════════════════╝"<<endl;
 				system("pause");
 				system("cls");
 				break;
 			case 2:
-				cout<<"Ejecutando caso 2..."<<endl;
+				cout<<"╔═════════════════════════════════════════╗"<<endl;
+				cout<<"║          Ejecutando caso 2...           ║"<<endl;
+				cout<<"╚═════════════════════════════════════════╝"<<endl;
 				system("pause");
 				system("cls");
 				break;
 			case 3:
-				cout<<"Ejecutando caso 3..."<<endl;
+				cout<<"╔═════════════════════════════════════════╗"<<endl;
+				cout<<"║          Ejecutando caso 3...           ║"<<endl;
+				cout<<"╚═════════════════════════════════════════╝"<<endl;
 				system("pause");
 				system("cls");
 				break;
 			default:
-				cout<<"Ejecutando caso default..."<<endl;
-				system("pause");
 				system("cls");
 				break;
 		}
