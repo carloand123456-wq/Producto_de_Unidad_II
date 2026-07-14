485164d27dbc91ae486562c4afe9c564861f70f1
carloand123456-wq <carloand123456@gmail.com>
2026-07-14 17:36:48 -0500

    Se agregaron las cabeceras de las funciones.

--- Proyecto/menu-r.h
+++ Proyecto/menu-r.h
@@ -1,8 +1,9 @@
 #ifndef MENU_R_H
 #define MENU_R_H
+#include "funciones_r.h"
 
-void menu();
-void menu2();
-void menu3();
+void menu(Productos PT[], int &cantProductos, Venta VT[], int &cantVentas);
+void menu2(Productos PT[], int &cantProductos);
+void menu3(Productos PT[], int cantProductos);
 
 #endif
